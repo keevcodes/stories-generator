7da5c8de2cdd7fc387b3b4825f317fed6e2adcac
keevkeev <andmckvr@gmail.com>
2019-06-09 09:12:55 +0200

    🐛 moved users controller logic to user controller
    User controller logic was previously in index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,38 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
-
-const formController = require('./createUser');
+const user = require('./createUser');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'StoriesLab.io' });
 });
 
-router.get('/users', (req, res, next) => {
-  res.render('users', {title: 'users page'})
-});
-
-
-router.post('/users', [
-  check('username').isAlphanumeric(),
-  check('password').isLength({min: 6})
-], (req, res, next) => {
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(442).json({ errors: errors.array() });
-  } else {
-    const user = {
-      username: req.body.username,
-      password: req.body.password,
-    }
-    formController.logRes(user);
-    res.redirect('/users');
-    res.end();
-  }
-})
+user;
 
 module.exports = router;
