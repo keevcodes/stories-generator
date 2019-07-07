d2a77030eef9fdc81f2fbf500caf82c0ae9cf298
keevkeev <andmckvr@gmail.com>
2019-07-07 14:46:55 +0200

    ✨ logic for file type and data fetching on drop
    Only three types of files are supported for now, however file types that are dropped into the zone are now fetched as a file and can be snet to the backend

--- public/scripts/dragDropHandler.js
+++ public/scripts/dragDropHandler.js
@@ -1,10 +1,20 @@
 function dragoverHandler(event) {
   event.preventDefault();
-  console.log('drag over');
   event.dataTransfer.dropEffect = 'move';
 }
 
+const VALID_FILE_TYPES = ['image/jpeg', 'image/png', 'video/mp4'];
+
 function dropHandler(event) {
   event.preventDefault();
-  console.log('drop', event);
+  let data = event.dataTransfer.items
+  for (let i = 0; i < data.length; i++) {
+    if (VALID_FILE_TYPES.includes(data[i].type)) {
+      const asFile = data[i].getAsFile();
+      console.log(data[i].type, data[i].kind, asFile);
+    } else {
+      window.alert("please make sure all files provided are either an image (jpeg or png) or a video (mp4)");
+    }
+  }
+  console.log('drop');
 }
