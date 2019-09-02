2d3ed810313c66fa88651e1529f2028d91df8174
keevkeev <andmckvr@gmail.com>
2019-09-02 17:15:51 +0200

    ✨ handle drag and drop post with form
    previous drag and drop implementation was overly complex and could be simplifed via a form input field and simple submit call

--- public/scripts/dragDropHandler.js
+++ public/scripts/dragDropHandler.js
@@ -1,20 +1,6 @@
-function dragoverHandler(event) {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'move';
-}
+const form = document.forms.dropForm;
 
-const VALID_FILE_TYPES = ['image/jpeg', 'image/png', 'video/mp4'];
-
-function dropHandler(event) {
-  event.preventDefault();
-  let data = event.dataTransfer.items
-  for (let i = 0; i < data.length; i++) {
-    if (VALID_FILE_TYPES.includes(data[i].type)) {
-      const asFile = data[i].getAsFile();
-      console.log(data[i].type, data[i].kind, asFile);
-    } else {
-      window.alert("please make sure all files provided are either an image (jpeg or png) or a video (mp4)");
-    }
-  }
-  console.log('drop');
+//TODO: add conditions for incorrect data types and empty forms
+function dropHandler() {
+  form.submit();
 }
