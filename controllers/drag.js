db3f4d88e384ee3531a7de0694c237ed0259de75
keevkeev <andmckvr@gmail.com>
2019-09-02 17:38:25 +0200

    ✨ create amp-story.html file with drag controller
    drag controller now generates a data.json file with the image data along with an amp-story.html file that passes image data into the download.pug view file.

---
+++ controllers/drag.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const Pug = require('pug');
+
+
+const source = './views/download.pug';
+const template = Pug.compileFile(source);
+
+exports.drag = (req, res, next) => {
+  /**
+   *  data needs to be stringified via JSON as fs.writeFile()
+   *  writeFile() expects a sting as second param.
+   *  https://nodejs.org/api/fs.html#fs_fs_write_fd_buffer_offset_length_position_callback
+   *  https://stackoverflow.com/questions/21976567/write-objects-into-file-with-node-js
+   */
+  const stringified = JSON.stringify(req.body);
+
+  fs.writeFile('./data/data.json', stringified, 'utf8', (err) => {
+    if (err) {
+      console.log('error writing media data to json file, drag.js', err);
+    }
+  });
+
+  /**
+   *  parse JSON string and pass to pug template.
+   *
+   */
+  const ampHtml = template(JSON.parse(stringified));
+
+  fs.writeFile('./amp-story.html', ampHtml, (err) => {
+    if (err) {
+      console.log('error writing story.html file', err)
+    } else {
+      console.log('success writing story.html')
+    }
+  });
+
+  res.download('./amp-story.html', (err) => {
+    if(err) {
+      console.log('error in downloading amp story')
+    } else {
+      console.log('downloading amp story')
+    }
+  });
+};
